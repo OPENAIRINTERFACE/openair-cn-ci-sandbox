47eef71bbfde58129419bcaab168d0e3a2d4f654
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2018-04-25 14:50:59 +0200

    Adding new files to develop to check when pull request is done
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

---
+++ src/test2.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright OAI Software Alliance
+ * Author Raphael Defosseux
+ * Comments : Proper comments
+ */
+#include <stdio.h>
+
+int main()
+{
+    int testId = 1;
+    printf ("Test%d\n", testId);
+    return 0;
+}
