6e5371428962e56babec58607243eeec304bc7a4
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2019-01-22 13:52:09 +0100

    Touching the file to create a PR
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/test1.c
+++ src/test1.c
@@ -12,7 +12,7 @@ int main()
 {
 // Intentional warning for CPPCHECK
     unsigned int testId = 1;
-    printf ("Test%d\n", testId);
+    printf ("Test%d for PR\n", testId);
 // Debug Variant
 #ifdef _DEBUG
 // Intentional error for CPPCHECK
